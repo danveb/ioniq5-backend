bf585b8df430557a86e6a5a8d213da9cf5ffab75
Daniel Bae <danibae91@icloud.com>
2022-02-16 16:59:21 -0800

    minor tweaks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express') 
-const axios = require('axios') 
-const cors = require('cors') 
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
 const app = express()
 
 const ExpressError = require("./expressError")
@@ -18,7 +18,7 @@ app.get('/finder', async (req, res) => {
     const model = req.query.model
     const radius = req.query.radius
 
-    const url = "https://www.hyundaiusa.com/var/hyundai/services/inventory/vehicleList.json?zip=" + zip + "&year=" + year + "&model=" + model + "&radius=" + radius 
+    const url = "https://www.hyundaiusa.com/var/hyundai/services/inventory/vehicleList.json?zip=" + zip + "&year=" + year + "&model=" + model + "&radius=" + radius
 
     const INVENTORY_REFERER_HEADER = "https://www.hyundaiusa.com/us/en/vehicles"
 
@@ -30,12 +30,12 @@ app.get('/finder', async (req, res) => {
     }
 
     const response = await axios.get(url, options)
-    const { data } = response.data 
-    res.send(data) 
+    const { data } = response.data
+    res.send(data)
 })
 
 /** 404 handler */
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     const err = new ExpressError("Not Found", 404);
     return next(err);
 });
@@ -43,14 +43,14 @@ app.use(function(req, res, next) {
 /** general error handler */
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
-  
+
     return res.json({
-      error: err,
-      message: err.message
+        error: err,
+        message: err.message
     });
 });
 
 /** Server startup */
-app.listen(3000, function () {
-  console.log("Listening on 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on 3000");
 });
